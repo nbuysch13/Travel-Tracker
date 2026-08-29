2d555bc25d26bdf9468c2c6a502b0f3e174a2eec
nbuysch13 <n.buysch@gmail.com>
2026-08-29 16:29:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+/* Bei jedem neuen Upload die Zahl erhöhen, dann laden alle Geräte die neue Version. */
+const CACHE = "reisepass-v1";
+const FILES = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
